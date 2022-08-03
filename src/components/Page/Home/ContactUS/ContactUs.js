01998c040da792dc38a5e7caa65395a0f93542af
Foy5al <mh.foysal.h@gmail.com>
2022-08-04 01:26:02 +0600

    update3

--- src/components/Page/Home/ContactUS/ContactUs.js
+++ src/components/Page/Home/ContactUS/ContactUs.js
@@ -27,7 +27,7 @@ const ContactUs = () => {
                                     <h1 className='imageIcon p-3 px-4 rounded-circle'><i className="fa-solid fa-envelope articleIcon"></i></h1>
                                 </div>
                                 <h4 className='flexTitle text-center'>Email:</h4>
-                                <p className='text-center flexParagraph mt-0 paragraph'>mh.foysal.h@gmail.com <br /> mh.foysal.h@gmail.com</p>
+                                <p className='text-center flexParagraph mt-0 paragraph'>mh.foysal.h@gmail.com </p>
                             </div>
 
                             <div className="col">
@@ -54,7 +54,7 @@ const ContactUs = () => {
                         </div>
                     </div>
                     <div className='col-12  col-lg-6 order-2 mapHeight p-3 p-lg-5'>
-                        <iframe title='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d322.7487136290176!2d90.43382673513315!3d23.765742514165627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7a7c3040f01%3A0x2241154b881cd8cd!2sSouth%20anandanagar%20water%20pump!5e0!3m2!1sen!2sbd!4v1653309051531!5m2!1sen!2sbd" className='w-100 h-100 zoomToFit'></iframe>
+                        <iframe title='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d228.21814718087563!2d90.4093199751501!3d23.765546865050883!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x45ddac7c16b4e43b!2zUmVraGFjaXRybyAtIOCmsOCnh-CmluCmvuCmmuCmv-CmpOCnjeCmsA!5e0!3m2!1sen!2sbd!4v1659552863549!5m2!1sen!2sbd" className='w-100 h-100 zoomToFit'></iframe>
                     </div>
 
                 </div>
